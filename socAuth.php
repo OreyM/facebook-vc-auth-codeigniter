2fb2ff95f3a0da596325e113c9e7bb8fea290f3d
Makarov Sergii <oreymgt@gmail.com>
2018-01-12 11:45:25 +0200

    add private function

--- socAuth.php
+++ socAuth.php
@@ -30,4 +30,58 @@ Class LoginSocial Extends CI_Controller{
         }
     }
 
+    private function setTokenData($tokenLink){
+        $this->socialToken = $tokenLink;
+    }
+
+    private function getTokenData(){
+        return json_decode(file_get_contents($this->socialToken), true);
+    }
+
+    private function setUserData($userLink){
+        $this->userData = $userLink;
+    }
+
+    private function getUserData(){
+        return json_decode(file_get_contents($this->userData), true);
+    }
+
+    private function setSessionData(array $userData){
+        if(count($userData) > 1){
+            #Facebook user data
+            if (!isset($userData['email']))
+                $userFB['email'] = 'noMail';
+            $this->sessionData = [
+                'user'      => $userData['name'],
+                'id'        => $userData['id'],
+                'email'     => $userData['email']
+            ];
+        }
+        else{
+            #VC user data
+            foreach ($userData as $firstIndex) {
+                foreach ($firstIndex as $dataVC) {
+                    $this->sessionData = [
+                        'user'     => $dataVC['first_name'].' '.$dataVC['last_name'],
+                        'id'       => $dataVC['uid'],
+                        'email'    => 'noMail'
+                    ];
+                }
+            }
+        }
+    }
+
+    private function getSessionData(){
+        return $this->sessionData;
+    }
+
+    private function newUserAutorization(array $sessionData){
+        $newUserData['user_name']   = $sessionData['user'];
+        $newUserData['user_id']     = $sessionData['id'];
+        $newUserData['user_mail']  = $sessionData['email'];
+        $newUserData['user_ip']     = $this->input->ip_address();
+
+        $this->loginModel->addNewUserInDB($newUserData);
+    }
+
 }
